a72e95e7f8a0f4cc26e07c094059367872bdd761
m-rejdych <rejdych.m.r@gmail.com>
2022-06-17 16:45:01 +0200

    Updated main theme colors.

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -4,19 +4,19 @@ local nord = {
 	dark_gray = "#3B4252", -- nord1 in palette
 	gray = "#434C5E", -- nord2 in palette
 	light_gray = "#4C566A", -- nord3 in palette
-	light_gray_bright = "#616E88", -- out of palette
+	light_gray_bright = "#506477", -- out of palette
 	darkest_white = "#D8DEE9", -- nord4 in palette
 	darker_white = "#E5E9F0", -- nord5 in palette
 	white = "#ECEFF4", -- nord6 in palette
-	teal = "#8FBCBB", -- nord7 in palette
-	off_blue = "#88C0D0", -- nord8 in palette
-	glacier = "#81A1C1", -- nord9 in palette
-	blue = "#5E81AC", -- nord10 in palette
+	teal = "#5DE4C7", -- nord7 in palette
+	off_blue = "#A6ACCD", -- nord8 in palette
+	glacier = "#ADD7FF", -- nord9 in palette
+	blue = "#7390AA", -- nord10 in palette
 	red = "#BF616A", -- nord11 in palette
 	orange = "#D08770", -- nord12 in palette
 	yellow = "#EBCB8B", -- nord13 in palette
-	green = "#A3BE8C", -- nord14 in palette
-	purple = "#B48EAD", -- nord15 in palette
+	green = "#5FB3A1", -- nord14 in palette
+	purple = "#D0679D", -- nord15 in palette
 	none = "NONE",
 }
 return nord
